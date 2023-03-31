3138b9d25ad960fa723b6d97c691731c0f60f2e3
allamymp <allamympereira@gmail.com>
2023-03-30 23:59:10 -0300

    function login implemented

--- BDD/front-end-project/script/script.js
+++ BDD/front-end-project/script/script.js
@@ -1,15 +1,22 @@
-function login() {
-  var emailInput = document.getElementById('emailForm').value;
-  var passwordInput = document.getElementById('passwordForm').value;
-  
-  for (var i = 0; i < users.length; i++) {
-    var user = users[i];
-    
-    if (user.email === emailInput && user.password === passwordInput) {
-      window.location.href = "dashboard.html";
-      return;
-    }
-  }
-  
-  alert("Incorrect email or password. Try again.");
+const form = document.querySelector('form');
+const btnLogin = document.querySelector('#btn-login');
+
+btnLogin.addEventListener('click', login);
+
+function login(event) {
+  event.preventDefault();
+
+  const email = document.querySelector('#emailForm').value;
+  const senha = document.querySelector('#passwordForm').value;
+
+  fetch('./DB/registerTest.json')
+    .then(response => response.json())
+    .then(data => {
+      if (email === data.login && senha === data.senha) {
+        alert('Login realizado com sucesso!');
+      } else {
+        alert('Email ou senha incorretos!');
+      }
+    })
+    .catch(error => console.error(error));
 }
