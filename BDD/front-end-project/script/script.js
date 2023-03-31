0a7cc2a6520f05268c2450384a919b2d02119a1a
allamymp <allamympereira@gmail.com>
2023-03-30 23:07:39 -0300

    db folder created, login funcition implemented

--- BDD/front-end-project/script/script.js
+++ BDD/front-end-project/script/script.js
@@ -1,3 +1,15 @@
-function test() {
-    alert("test passed!");
+function login() {
+  var emailInput = document.getElementById('emailForm').value;
+  var passwordInput = document.getElementById('passwordForm').value;
+  
+  for (var i = 0; i < users.length; i++) {
+    var user = users[i];
+    
+    if (user.email === emailInput && user.password === passwordInput) {
+      window.location.href = "dashboard.html";
+      return;
+    }
   }
+  
+  alert("Incorrect email or password. Try again.");
+}
