758c97dddc5a3197ad9c818f60ae1b2d09bccfcb
allamymp <allamympereira@gmail.com>
2023-04-03 10:46:00 -0300

    exercise finished

--- Algorithms/Algorithms/src/exercise-03/Arrangement.h
+++ Algorithms/Algorithms/src/exercise-03/Arrangement.h
@@ -1,25 +1,46 @@
-template <class T>
+template<class T>
 class Arrangement {
 private:
-int tamanho; // tamanho do arranjo
-T * items; //items do arranjo
+	int tamanho; // tamanho do arranjo
+	T *items; //items do arranjo
 public:
-Arrangement(int tam) {
-// instanciar o array de items com new (pratica 1) e setar tamanho;
-}
-virtual ~Arrangement() {
-// destruir o array de items (prática 1);
-}
-virtual T get(int idx) {
-// retornar um item do array a partir do indice;
-}
-virtual void set(int idx, const T & item) {
-// set o item do array apontado pelo indice usando =
-}
-virtual void exibir();
+	Arrangement(int tam) {
+		this->tamanho = tam;
+		items = new T[tam];
+		// instanciar o array de items com new (pratica 1) e setar tamanho;
+	}
+	virtual ~Arrangement() {
+		delete[] items;
+		// destruir o array de items (prática 1);
+	}
+	virtual T get(int idx) {
+		if (idx < 0 || idx >= tamanho) {
+			throw std::out_of_range("Índice fora do intervalo válido.");
+		}
+		return items[idx];
+		// retornar um item do array a partir do indice;
+	}
+	virtual void set(int idx, const T &item) {
+		if (idx < 0 || idx >= tamanho) {
+			throw std::out_of_range("Índice fora do intervalo válido.");
+		}
+		items[idx] = item;
+		// set o item do array apontado pelo indice usando =
+	}
+	virtual void exibir();
 };
+
+class InvalidIndex: public std::out_of_range {
+public:
+	InvalidIndex(const std::string &message) :
+			std::out_of_range(message) {
+	}
+};
+
 template<class T>
 void Arrangement<T>::exibir() {
-	// exibir cada item numa linha da forma "<idx>: <item>"
+	for (int i = 0; i < tamanho; i++) {
+		std::cout << "[" << i << ": " << items[i] << "] ";
 	}
-
+	// exibir cada item numa linha da forma "<idx>: <item>"
+}
--- Algorithms/Algorithms/src/exercise-03/Pratice03.cpp
+++ Algorithms/Algorithms/src/exercise-03/Pratice03.cpp
@@ -7,20 +7,53 @@
 //============================================================================
 
 #include <iostream>
-
 #include "Functions.h"
 using namespace std;
 using namespace functions;
+#include "Arrangement.h"
+#include "Student.h"
 
-#include "Functions.h"
 
 int main() {
-	float x = 5.5, y = 10.15, z = 30.7;
-	cout << "Antes 	: x = " << x <<  " y = " << y << endl;
-	trocar(x, y);
-	cout << "Depois	: x = " << x << " y = " << y << endl;
-	cout << "Minimo de \"strA\" e \"strB\": " << minimo("strA", "strB") << endl;
-	cout << "Maximo de \"strA\" e \"strB\": " << maximo("strA", "strB") << endl;
+	try {
+
+		float x = 5.5, y = 10.15, z = 30.7;
+		cout << "Antes 	: x = " << x << " y = " << y << endl;
+		trocar(x, y);
+		cout << "Depois	: x = " << x << " y = " << y << endl;
+		cout << "Minimo de \"strA\" e \"strB\": " << minimo("strA", "strB")
+				<< endl;
+		cout << "Maximo de \"strA\" e \"strB\": " << maximo("strA", "strB")
+				<< endl;
+//
+		cout << endl << "intArr:";
+		Arrangement<int> intArr(10);
+		intArr.set(4, 5);
+		intArr.set(7, 15);
+		intArr.set(8, 22);
+		intArr.exibir();
+		cout << endl << "floarArr:";
+		Arrangement<float> floatArr(5);
+		floatArr.set(0, 1.1);
+		floatArr.set(1, 1.2);
+		floatArr.set(2, 1.3);
+		floatArr.set(3, 1.4);
+		floatArr.set(4, 1.5);
+		floatArr.exibir();
+//
+		//intArr.get(10);
+		cout << endl << "classroom:";
+		Arrangement<Student> classroom(3);
+		classroom.set(0, Student("Joao", "1234"));
+		classroom.set(1, Student("Maria", "5235"));
+		classroom.set(2, Student("Jose", "2412"));
+		classroom.exibir();
+	} catch (InvalidIndex &e) {
+		cout << "Erro: " << e.what() << endl;
+	}
 
 	return 0;
 }
+
+
+
